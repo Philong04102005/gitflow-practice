4cad8af80587bed9cd50135cdae29582ff8f81da
LongNPP <philong04102005@gmail.com>
2025-06-16 00:13:40 +0700

    fix bug to release

--- login.py
+++ login.py
@@ -3,3 +3,6 @@ passWord = input("Enter your password: ")
 
 if userName == "admin" and passWord == "password123":
     print("Login successful!")
+else:
+    print("Login failed! Please check your username and password.")
+    exit()
